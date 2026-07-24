394d49d4c8cc20fd7cfce8042ba3e66cfa196d52
Pasquale Mainolfi <mnlpql@gmail.com>
2026-07-25 00:51:47 +0200

    remove build

--- build/CMakeFiles/watch_release_package.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/watch_release_package"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/watch_release_package.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
